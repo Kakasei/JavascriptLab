2ec6580486ea49c13457f4c54cbc4c365d4af7cd
Kakasei <751832214@qq.com>
2025-11-24 01:06:09 +0800

    完成746

---
+++ leetcode/746-使用最小花费爬楼梯.ts
@@ -0,0 +1,12 @@
+// 动态规划基础题，递推写法
+// 但是没有优化空间复杂度
+// TODO
+function minCostClimbingStairs(cost: number[]): number {
+    const f = new Array(cost.length + 1);
+    f[0] = 0;
+    f[1] = 0;
+    for (let i = 2; i <= cost.length; i++) {
+        f[i] = Math.min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+    }
+    return f[cost.length];
+}
